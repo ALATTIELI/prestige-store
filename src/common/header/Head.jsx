597b322e89c9e6b823ea57b4085b16c3c7932704
adila902m <adila902@yahoo.com>
2022-07-05 21:14:02 +0400

    first commit

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +971(0)26411168 </label>
+            <i className='fa fa-envelope'></i>
+            <label> info@prestigestore.ae </label>
+          </div>
+          <div className='right row RText'>
+            <span>🌍</span>
+            <label>EN/AR</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
