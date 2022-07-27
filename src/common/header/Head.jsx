32dd032ae7221aec701dc60c3742b138aa2db8d1
adila902m <adila902@yahoo.com>
2022-07-27 22:04:41 +0400

    ..

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -1,24 +1,27 @@
-import React from "react"
+import React from "react";
+import Flag from "react-world-flags";
 
 const Head = () => {
   return (
     <>
-      <section className='head'>
-        <div className='container d_flex'>
-          <div className='left row'>
-            <i className='fa fa-phone'></i>
+      <section className="head">
+        <div className="container d_flex">
+          <div className="left row">
+            <i className="fa fa-phone"></i>
             <label> +971529744450</label>
-            <i className='fa fa-envelope'></i>
+            <i className="fa fa-envelope"></i>
             <label> info@prestigestore.ae </label>
           </div>
-          <div className='right row RText'>
-            <span>🌍</span>
-            <label>EN/AR</label>
+          <div className="right row RText">
+            <i><Flag code="gb" height={10}/></i> 
+            <label>EN</label>
+            <i><Flag code="ae" height={10}/></i> 
+            <label>AR</label>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Head
+export default Head;
--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,40 +1,45 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Search = ({ CartItem }) => {
   // fixed Header
   window.addEventListener("scroll", function () {
-    const search = document.querySelector(".search")
-    search.classList.toggle("active", window.scrollY > 100)
-  })
+    const search = document.querySelector(".search");
+    search.classList.toggle("active", window.scrollY > 100);
+  });
 
   return (
     <>
-      <section className='search'>
-        <div className='container c_flex'>
-          <div className='logo width '>
+      <section className="search">
+        <div className="container c_flex">
+          <div className="logo width ">
             <h1>Prestige Store</h1>
           </div>
 
-          <div className='search-box f_flex'>
-            <i className='fa fa-search'></i>
-            <input type='text' placeholder='Search and hit enter...' />
+          <div className="search-box f_flex">
+            <i className="fa fa-search"></i>
+            <input type="text" placeholder="Search and hit enter..." />
             <span>All Category</span>
           </div>
 
-          <div className='icon f_flex width'>
-            <i className='fa fa-user icon-circle'></i>
-            <div className='cart'>
-              <Link to='/cart'>
-                <i className='fa fa-shopping-bag icon-circle'></i>
+          <div className="icon f_flex width">
+            <i className="fa fa-user icon-circle"></i>
+            <div className="cart">
+              <Link to="/cart">
+                <i className="fa fa-shopping-bag icon-circle"></i>
                 <span>{CartItem.length === 0 ? "" : CartItem.length}</span>
               </Link>
             </div>
+            <i className="icon-circle">
+              <a href="https://wa.me/971505451140">
+                <img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png" />
+              </a>
+            </i>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
