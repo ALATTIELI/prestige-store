a22bddfec16e17c2c92d7d5a6e30282603f0dcd0
Bilal Akkil <bilal0akkil@gmail.com>
2022-08-23 19:53:26 +0400

    Add search suggestion.

--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -16,10 +16,49 @@ const Search = ({ CartItem }) => {
             <h1>Prestige Store</h1>
           </div>
 
-          <div className="search-box f_flex">
-            <i className="fa fa-search"></i>
-            <input type="text" placeholder="Search and hit enter..." />
-            <span>All Category</span>
+          <div className="search-box_container">
+            <div className="search-box f_flex">
+              <i className="fa fa-search"></i>
+              <input type="text" placeholder="Search and hit enter..." />
+              <span>All Category</span>
+            </div>
+            <div className="suggestion">
+              <ul>
+              <li className="sugg_listItem">
+                  <span>Samsung</span>
+                </li>
+                <li className="sugg_listItem">
+                  <span>???</span>
+                </li>
+                <li className="sugg_listItem">
+                  <span>Iphone</span>
+                </li>                <li className="sugg_listItem">
+                  <span>Samsung</span>
+                </li>
+                <li className="sugg_listItem">
+                  <span>???</span>
+                </li>
+                <li className="sugg_listItem">
+                  <span>Iphone</span>
+                </li>                <li className="sugg_listItem">
+                  <span>Samsung</span>
+                </li>
+                <li className="sugg_listItem">
+                  <span>???</span>
+                </li>
+                <li className="sugg_listItem">
+                  <span>Iphone</span>
+                </li>                <li className="sugg_listItem">
+                  <span>Samsung</span>
+                </li>
+                <li className="sugg_listItem">
+                  <span>???</span>
+                </li>
+                <li className="sugg_listItem">
+                  <span>Iphone</span>
+                </li>
+              </ul>
+            </div>
           </div>
 
           <div className="icon f_flex width">
@@ -32,7 +71,10 @@ const Search = ({ CartItem }) => {
             </div>
             <i className="icon-circle">
               <a href="https://wa.me/971529744450">
-                <img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png" alt="whatsapp-logo"/>
+                <img
+                  src="https://img.icons8.com/color/48/000000/whatsapp--v1.png"
+                  alt="whatsapp-logo"
+                />
               </a>
             </i>
           </div>
