59dc0bc8375a6c33985b78451232a66e7cfdada0
adila902m <adila902@yahoo.com>
2022-08-31 16:57:47 +0400

    add google and facebook login buttons

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./login.css"
+
+export default function Login() {
+  return (
+    <div>
+      <div className='google-login-button'>Sing in with Google</div>
+      <div className='facebook-login-button'>Sing in with Facebook</div>
+    </div>
+  )
+}
