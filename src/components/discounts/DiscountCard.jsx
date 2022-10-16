e095968e30ffce1b80b0103cb6938dc65809b258
bil0ak <bilal0akkil@gmail.com>
2022-10-16 17:55:49 +0400

    load data from backend

--- src/components/discounts/DiscountCard.jsx
+++ src/components/discounts/DiscountCard.jsx
@@ -1,19 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  getDiscountById,
+  getImageById,
+  getLatestDiscounts,
+} from "../../redux/apiCalls";
+
+// const BASE_URL = "http://localhost:3000/api/v1";
+async function getImageUrl(id) {
+  const url = getImageById(id);
+  return url;
+}
 
 const DiscountCard = () => {
-  const productItems = [
-    {
-      id: 1,
-      name: "Product 1",
-      price: 100,
-      quantity: 1,
-    },
-  ];
+  const [productItems, setProductItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(count + 1);
-  };
+  const [discounts, setDiscounts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await getLatestDiscounts();
+        console.log(res);
+        if (res !== null) {
+          // res.map(async (item) => {
+          //   const discount = await getDiscountById(item.discountID);
+          //   const discount_percentage = discount.discount_percentage;
+          //   const newItem = { ...item, discount_percentage };
+          //   setProductItems((prev) => [...prev, newItem]);
+          // });
+          setProductItems(res);
+          setLoading(false);
+        } else {
+          setLoading(true);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
+
+  async function getDiscount(id) {
+    const discount = await getDiscountById(id);
+    const discount_percentage = discount.discount_percentage;
+    console.log(discount_percentage);
+    return discount_percentage;
+  }
 
   const scroll = (direction) => {
     const discount_card = document.getElementById("discount_card_container");
@@ -38,37 +71,46 @@ const DiscountCard = () => {
           <i className="fa fa-long-arrow-alt-left"></i>
         </span>
         <div className="discount_card_container" id="discount_card_container">
-          {productItems.map((productItems) => {
-            return (
-              <div className="discount_card_product">
-                <div className="discount_card_product_img">
-                  <span className="top_left_popup">{productItems.discount}% Off</span>
-                  <img src={productItems.cover} alt="" />
-                </div>
-                <div className="discount_card_product_details">
-                  <h3>{productItems.name}</h3>
-                  <div className="discount_card_product_price">
-                    <div className="DiscountPrice">
-                      <span className="DiscountDPrice">
-                        AED {productItems.price}
-                      </span>
-                      <span
-                        className="DiscountOriginalPrice"
-                        style={{ textDecoration: "line-through" }}
+          {loading ? (
+            <div className="loading">No Discounts</div>
+          ) : (
+            productItems.map((productItem) => {
+              return (
+                <div className="discount_card_product">
+                  <div className="discount_card_product_img">
+                    <span className="top_left_popup">
+                      {async() => {
+                        return await getDiscount(productItem.discountID);
+                      }}
+                      % Off
+                    </span>
+                    <img src={getImageById(productItem.images[0])} alt="" />
+                  </div>
+                  <div className="discount_card_product_details">
+                    <h3>{productItem.name_en}</h3>
+                    <div className="discount_card_product_price">
+                      <div className="DiscountPrice">
+                        <span className="DiscountDPrice">
+                          AED {productItem.TotalPrice}
+                        </span>
+                        <span
+                          className="DiscountOriginalPrice"
+                          style={{ textDecoration: "line-through" }}
+                        >
+                          AED {productItem.price}
+                        </span>
+                      </div>
+                      <button
+                      // onClick={() => addToCart(productItems)}
                       >
-                        AED {productItems.originalprice}
-                      </span>
+                        <i className="fa fa-plus"></i>
+                      </button>
                     </div>
-                    <button 
-                    // onClick={() => addToCart(productItems)}
-                    >
-                      <i className="fa fa-plus"></i>
-                    </button>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })
+          )}
         </div>
         <span
           className="next"
