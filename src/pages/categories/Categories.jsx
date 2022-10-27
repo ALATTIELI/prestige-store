3baad11c7039fd55bb06aca34ab145956fabb3bd
bil0ak <bilal0akkil@gmail.com>
2022-10-27 23:06:07 +0400

    fetch categories

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -1,34 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import { getCategories, getImageById } from "../../redux/apiCalls";
 import "./categories.css";
 
 const Categories = () => {
+  const { t, i18n } = useTranslation();
+  const [productItems, setProductItems] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await getCategories();
+        console.log(res);
+        if (res !== null) {
+          setProductItems(res);
+          setLoading(false);
+        } else {
+          setLoading(true);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const handleClick = (id) => {
+    navigate(`/category/${id}`);
+  };
+
   return (
-    <div className="cate_card">
-      <div className="cate_body_card">
-        <img className="cate_img_card" src="https://cdn.shopify.com/s/files/1/0323/0593/8570/products/iphone-13-mini-product-red-select-2021.png?v=1634661101"/>
-        <h2 className="cate_title_card">Mobiles</h2>
-      </div>
-      <div className="cate_body_card">
-        <img className="cate_img_card" src="https://www.freepnglogos.com/uploads/macbook-png/macbook-cleanmymac-the-best-mac-cleanup-app-for-macos-get-32.png"/>
-        <h2 className="cate_title_card">Computers & laptops</h2>
-      </div>
-      <div className="cate_body_card">
-        <img className="cate_img_card" src="https://www.pngall.com/wp-content/uploads/5/Samsung-TV-PNG-Image-HD.png"/>
-        <h2 className="cate_title_card">TVs</h2>
-      </div>
-      <div className="cate_body_card">
-        <img className="cate_img_card" src="https://mm.jbl.com/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw40cc9388/450BT_black_angle_01-1606x1606px.png"/>
-        <h2 className="cate_title_card">Headphones & Speakers</h2>
-      </div>
-      <div className="cate_body_card">
-        <img className="cate_img_card" src="https://gmedia.playstation.com/is/image/SIEPDC/ps5-more-features-ps5-ps4-pro-image-block-01-en-22oct20?$native--t$"/>
-        <h2 className="cate_title_card">Gaming</h2>
-      </div>
-      <div className="cate_body_card">
-        <img className="cate_img_card" src="https://i.pinimg.com/originals/7f/dd/3b/7fdd3bda2076f7bdb2ed96e7ec01810a.png"/>
-        <h2 className="cate_title_card">Cameras</h2>
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <div className="loading">
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <div className="cate_card">
+          {productItems &&
+            productItems.map((productItem) => {
+              return (
+                <div
+                  className="cate_card_item"
+                  key={productItem._id}
+                  onClick={() => handleClick(productItem._id)}
+                >
+                  <div className="cate_card_item_img">
+                    <img src={getImageById(productItem.image)} alt="" />
+                  </div>
+                  <div className="cate_card_item_name">
+                    <span>
+                      {i18n.language === "en"
+                        ? productItem.name_en
+                        : productItem.name_ar}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      )}
+    </>
   );
 };
 
