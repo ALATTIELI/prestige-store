11ba33bfa0d01b7669eab9c787e4a3c0188085d7
bil0ak <bilal0akkil@gmail.com>
2022-11-06 04:05:24 +0400

    change routes

--- src/App.js
+++ src/App.js
@@ -22,9 +22,41 @@ import ProductsPage from "./pages/productsPage/ProductsPage";
 import LoginPage from "./pages/loginPage/LoginPage";
 import CategoryPage from "./pages/categoryPage/CategoryPage";
 import BrandPage from "./pages/brandPage/BrandPage";
+import LoginSuccess from "./components/Login/LoginSuccess";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "./redux/userRedux";
+import LoginFailure from "./components/Login/LoginFailure";
 
 function App() {
-  var user = false;
+  const dispatch = useDispatch();
+  // check if localstorage expirate date is greater than current date
+  // if not, remove user from localstorage
+  if (localStorage.getItem("expirationTime")) {
+    // const user = JSON.parse(localStorage.getItem("user"));
+    const expirateDate = localStorage.getItem("expirationTime");
+    const currentDate = new Date().getTime();
+    // convert current milliseconds date to normal date
+    // for testing purposes
+    // const currentDatee = new Date().toUTCString();
+    // const expirateDatee = new Date(expirateDate*1).toUTCString();
+    // console.log("currentDatee: " + currentDatee);
+    // console.log("expirateDatee: " + expirateDatee);
+    // console.log(currentDate);
+    // console.log(expirateDate);
+    if (expirateDate < currentDate) {
+      console.log("user expired");
+      // create dispatch to logout user
+      dispatch(logout());
+    } else {
+      console.log("user is logged in");
+    }
+  }
+
+  const user = useSelector((state) => state.user.currentUser);
+  // if (user !== null) {
+  //   admin = user.isAdmin;
+  // }
+
   return (
     <>
       <Suspense fallback={<div>Loading...</div>}>
@@ -38,12 +70,12 @@ function App() {
             <Route path="/categories" element={<Categories />} />
             <Route path="/category/:id" element={<CategoryPage />} />
             {/* redirect /user to /user/profile */}
-            <Route path="/user" element={<User />}>
+            <Route path="/user">
               <Route path="" element={<UserProfile />} />
+              <Route path="profile" element={<UserProfile />} />
               <Route path="orders" element={<UserOrders />} />
               <Route path="order/:id" element={<Order />} />
             </Route>
-            <Route path="/user/profile" element={<UserProfile />} />
 
             <Route path="/user/orders" element={<UserOrders />} />
             <Route path="/user/order/:id" element={<Order />} />
@@ -65,6 +97,8 @@ function App() {
               path="/login"
               element={user ? <Navigate to="/" /> : <LoginPage />}
             />
+            <Route path="/loginSuccess" element={<LoginSuccess />} />
+            <Route path="/loginFailure" element={<LoginFailure />} />
           </Routes>
           <Footer />
         </BrowserRouter>
