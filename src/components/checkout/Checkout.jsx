f38dc4d02d8852c485113677acdbb84fe2fb7a14
bil0ak <bilal0akkil@gmail.com>
2022-11-27 22:26:22 +0400

    check if user is logged in or cart is empty

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -7,25 +7,37 @@ import "./checkout.css";
 import { useSelector } from "react-redux";
 import CheckoutForm from "./CheckoutForm";
 import AddressForm from "./AddressForm";
+import { useNavigate } from "react-router-dom";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 
 export default function Checkout() {
   const Cart = useSelector((state) => state.cart);
   const CartItems = Cart.products;
+  const navigate = useNavigate();
 
   document.title = "Checkout";
 
   const [clientSecret, setClientSecret] = useState("");
 
+  const user = useSelector((state) => state.user.currentUser);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    } else if (CartItems.length === 0) {
+      navigate("/cart");
+    }
+  }, [CartItems, user]);
+
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
     publicRequest
       .post("/checkout/create-payment-intent", {
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+        items: CartItems,
       })
       .then((data) => setClientSecret(data.data.clientSecret));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const appearance = {
